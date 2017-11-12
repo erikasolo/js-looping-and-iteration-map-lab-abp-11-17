af189a732869c53834ac8638eab43112bc977a5b
Erika Solomon <mrserikasolomon@gmail.com>
2017-11-12 18:20:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array){
+  return array.map(function(e)){
+    return e.toLowerCase()
+  })
+  
+}
+// 
+// {firstName: 'Bobby', lastName: 'Smith'}
