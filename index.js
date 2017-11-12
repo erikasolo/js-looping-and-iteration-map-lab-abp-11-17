e8d86936fc95ed2bd5cb7e0f1534dcfe2d4be724
Erika Solomon <mrserikasolomon@gmail.com>
2017-11-12 18:25:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file.
 const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
-function lowerCaseDrivers(array){
+function lowerCaseDrivers(drivers){
   return array.map(function(e)){
     return e. toLowerCase()
   })
