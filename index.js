01d65acaea2fb0ea69e7e3d4b57514951acae7bc
Erika Solomon <mrserikasolomon@gmail.com>
2017-11-12 18:31:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ function lowerCaseDrivers(drivers) {
  }
 //
 // {firstName: 'Bobby', lastName: 'Smith'}
+function nameToAttributes(drivers) {
+   return drivers.map(function(driver) {
+     let fName = driver.split(' ')[0];
+     let lName = driver.split(' ')[1];
+     return {firstName: fName, lastName: lName};
+   })
+ }
+ 
+function attributesToPhrase(drivers) {
+   return drivers.map(function(driver) {
+     return `${driver.name} is from ${driver.hometown}`;
+   });
+ }
